bc68ba451593c5ff162e28c0c4dfa25aa84dedbe
prashantmimpinge <prashantm.impinge@gmail.com>
2024-07-08 18:44:41 +0530

    my first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import Tesseract from 'tesseract.js';
+// import { PDFDocumentProxy } from 'pdf-lib';
+// import { getDocument, PDFDocumentProxy } from 'pdfjs-dist';
+
+// pdfjsLib.GlobalWorkerOptions.workerSrc = '//cdnjs.cloudflare.com/ajax/libs/pdf.js/2.6.347/pdf.worker.min.js';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularapp';
+
+  extractedText: string = '';
+
+ 
+  // async extractTextFromImage(image: File) {
+  //   const worker = Tesseract.createWorker();
+  //   await worker.load();
+  //   await worker.loadLanguage('eng');
+  //   await worker.initialize('eng');
+  //   const { data: { text } } = await worker.recognize(image);
+  //   this.extractedText = text;
+  //   console.log(text);
+  //   await worker.terminate();
+  // }
+
+  onFileChange(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      // this.extractTextFromImage(file);
+    }
+  }
+ 
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+// import pdfjsLib from 'pdfjs-dist/build/pdf';
 
+// // Set worker source
+// pdfjsLib.GlobalWorkerOptions.workerSrc = 'assets/pdf.worker.min.js'; // Adjust the path as per your project structure
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+  .catch((err:any) => console.error(err));
